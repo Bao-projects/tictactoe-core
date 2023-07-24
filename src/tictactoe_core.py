63fc534f44a31645193b4c72077857b07f3450f7
BaoThan <109579681+BaoThan@users.noreply.github.com>
2023-07-24 01:15:30 -0700

    Assignment01 (#2)
    Implemented is_board_full and check_winner

--- src/tictactoe_core.py
+++ src/tictactoe_core.py
@@ -4,6 +4,8 @@ This file contains the core functionality for the Tic Tac Toe game.
 from enum import Enum
 from typing import Optional
 
+BOARD_SIZE = 3
+
 
 class Player(Enum):
     X = "X"
@@ -17,14 +19,33 @@ def is_board_full(board: list[list[str]]) -> bool:
     Returns:
         True if the game board is full, false otherwise.
     """
-    return False
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            if board[i][j] == "":
+                return False
+    return True
 
 
 def check_winner(board: list[list[str]]) -> Optional[Player]:
     """
     Check if there is a winner for the given game board.
-
     Returns:
         None if there is no winner, otherwise returns the winning Player.
     """
+    # Check rows:
+    for i in range(BOARD_SIZE):
+        if board[i][0] == board[i][1] == board[i][2] != "":
+            return Player(board[i][0].upper())
+
+    # Check cols:
+    for j in range(BOARD_SIZE):
+        if board[0][j] == board[1][j] == board[2][j] != "":
+            return Player(board[0][j].upper())
+
+    # check diagonals:
+    if board[0][0] == board[1][1] == board[2][2] != "":
+        return Player(board[0][0].upper())
+    if board[0][2] == board[1][1] == board[2][0] != "":
+        return Player(board[0][2].upper())
+
     return None
--- test/test_tictactoe_core.py
+++ test/test_tictactoe_core.py
@@ -1,4 +1,5 @@
 from src.tictactoe_core import check_winner
+from src.tictactoe_core import is_board_full
 from src.tictactoe_core import Player
 
 
@@ -9,9 +10,43 @@ def test_check_winner() -> None:
     B2 = [["X", "", ""], ["", "", "O"], ["", "", ""]]
     assert check_winner(B2) == None
 
-    # Please write a few more test cases.
+    B3 = [["X", "X", "X"], ["", "", ""], ["O", "O", ""]]
+    assert check_winner(B3) == Player.X
+
+    B4 = [["X", "O", ""], ["X", "O", ""], ["X", "", ""]]
+    assert check_winner(B4) == Player.X
+
+    B5 = [["O", "O", ""], ["X", "X", "X"], ["O", "X", "O"]]
+    assert check_winner(B5) == Player.X
+
+    B6 = [["O", "X", "O"], ["X", "X", "O"], ["O", "O", "X"]]
+    assert check_winner(B6) == None
+
+    B7 = [["O", "", "X"], ["O", "X", ""], ["O", "", ""]]
+    assert check_winner(B7) == Player.O
+
+    B8 = [["X", "X", "O"], ["O", "X", "O"], ["X", "O", "X"]]
+    assert check_winner(B8) == Player.X
+
+    B9 = [["X", "O", "X"], ["O", "O", "X"], ["X", "X", "O"]]
+    assert check_winner(B9) == None
 
 
 def test_is_board_full() -> None:
-    # Please write your test here.
-    pass
+    B1 = [["X", "O", "X"], ["O", "X", "O"], ["O", "X", "O"]]
+    assert is_board_full(B1)
+
+    B2 = [["X", "O", ""], ["O", "", "O"], ["O", "X", "O"]]
+    assert not is_board_full(B2)
+
+    B3 = [["X", "", ""], ["", "O", ""], ["O", "", "X"]]
+    assert not is_board_full(B3)
+
+    B4 = [["X", "O", "X"], ["X", "O", "O"], ["O", "X", "O"]]
+    assert is_board_full(B4)
+
+    B5 = [["O", "O", "X"], ["X", "X", "O"], ["O", "X", "O"]]
+    assert is_board_full(B5)
+
+
+test_is_board_full()
