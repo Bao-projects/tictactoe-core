1ca6fe2d8b15931e07006cb707fc801c75f0b242
BaoThan <109579681+BaoThan@users.noreply.github.com>
2023-08-05 19:03:11 -0700

    Implement board_status api (#9)

--- src/routes/board_status.py
+++ src/routes/board_status.py
@@ -1,4 +1,13 @@
+import json
+
+from flask import request
+
 from . import routes
+from src.routes.helper.responses import BoardStatusResult
+from src.routes.helper.responses import FailureResult
+from src.tictactoe_core import check_winner
+from src.tictactoe_core import is_board_full
+from src.tictactoe_core import is_board_valid
 
 
 @routes.route("/board_status", methods=["GET"])
@@ -11,3 +20,14 @@ def board_status():
     who is the winner). In case the tic tac toe board is invalid, it will return
     an error message along with response code of 400
     """
+    board = json.loads(request.form["board"])
+    board_valid = is_board_valid(board)
+
+    if not board_valid:
+        return FailureResult("Invalid board").to_flask_response()
+
+    winner = check_winner(board)
+    has_winner = winner is not None
+    return BoardStatusResult(
+        is_board_full(board), has_winner, winner
+    ).to_flask_response()
