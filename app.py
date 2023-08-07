039fd5eee7a7c3a3ae651a0316e735143d057ed1
Luan Truong <truongluan303@gmail.com>
2023-08-07 16:19:41 -0700

    Add GUI for debugging purpose

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+import sys
+
 from flask import Flask
 
+from src.gui import run_gui
 from src.routes import *
 
 
 def main() -> None:
-    app = Flask(__name__)
-    app.register_blueprint(routes)
-    app.run()
+    if len(sys.argv) == 1:
+        app = Flask(__name__)
+        app.register_blueprint(routes)
+        app.run()
+    elif len(sys.argv) == 2 and sys.argv[1] in ("-g", "--gui"):
+        sys.exit(run_gui())
+    else:
+        print("ERROR: Unknown options!")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
