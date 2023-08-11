de88829e46fa98bc9f8c1649fdba83b354fb2ae5
BaoThan <bthan@sfsu.edu>
2023-08-11 00:07:36 -0700

    implement next_move api resovled after reviews

--- src/routes/best_next_move.py
+++ src/routes/best_next_move.py
@@ -6,6 +6,7 @@ from . import routes
 from src.routes.helper.responses import BestNextMoveResult
 from src.routes.helper.responses import FailureResult
 from src.tictactoe_core import best_next_move
+from src.tictactoe_core import is_board_valid
 
 
 @routes.route("/best_next_move", methods=["GET"])
@@ -19,8 +20,12 @@ def next_move():
         of that move from the result of best_next_move.
     """
     board = json.loads(request.form["board"])
+    board_valid = is_board_valid(board)
+    if not board_valid:
+        return FailureResult("Invalid board").to_flask_response()
+
     for_player = request.form["player"]
-    if for_player not in ["X", "O"]:
+    if for_player.upper() not in ["X", "O"]:
         return FailureResult("Invalid input").to_flask_response()
 
     next_move = best_next_move(board, for_player)
